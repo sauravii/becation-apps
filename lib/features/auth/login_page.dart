cf72307dbee98e80630a765e6c9bb946b94e8182
nined0se <orpingg.n@gmail.com>
2026-03-17 11:36:11 +0700

    Update login_page.dart

--- lib/features/auth/login_page.dart
+++ lib/features/auth/login_page.dart
@@ -1,345 +1,69 @@
-import 'package:becation_apps/features/auth/forgot_page.dart';
 import 'package:flutter/material.dart';
-import 'package:becation_apps/features/auth/register_page.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
+class Login extends StatefulWidget {
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  _LoginState createState() => _LoginState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginState extends State<Login> {
+
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
-  bool obscurePassword = true;
+  Future login() async {
 
-  String? emailError;
-  String? passwordError;
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    );
 
-  void validateLogin() {
-    setState(() {
-      emailError = null;
-      passwordError = null;
-
-      if (emailController.text.isEmpty) {
-        emailError = "Email is required.";
-      } else if (!emailController.text.contains("@")) {
-        emailError = "Please enter a valid email address.";
-      }
-
-      if (passwordController.text.isEmpty) {
-        passwordError = "Password is required.";
-      } else if (passwordController.text.length < 6) {
-        passwordError = "Password must be at least 6 characters long.";
-      }
-    });
-  }
-
-  Widget buildIcon(IconData icon) {
-    return Padding(
-      padding: EdgeInsets.only(right: 12.w),
-      child: Container(
-        width: 42.w,
-        height: 42.w,
-        decoration: BoxDecoration(
-          color: const Color(0xFF875DFC).withOpacity(0.15),
-          shape: BoxShape.circle,
-        ),
-        child: Icon(icon, color: const Color(0xFF875DFC), size: 22.sp),
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Home(),
       ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: const Color(0xFFFEF7FF),
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                child: IntrinsicHeight(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 24.w),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 90.h),
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
 
-                        /// HEADER
-                        Center(
-                          child: Column(
-                            children: [
-                              Text(
-                                "Welcome back",
-                                style: TextStyle(
-                                  fontSize: 28.sp,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: 6.h),
-                              Text(
-                                "Good to See You!",
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 14.sp,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
 
-                        SizedBox(height: 30.h),
+          children: [
 
-                        /// EMAIL LABEL
-                        Transform.translate(
-                          offset: Offset(0, 15.h),
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 54.w),
-                            child: Text(
-                              "Email Address",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 11.sp,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: 6.h),
-
-                        /// EMAIL FIELD
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                buildIcon(Icons.email),
-                                Expanded(
-                                  child: TextField(
-                                    controller: emailController,
-                                    decoration: InputDecoration(
-                                      border: const UnderlineInputBorder(),
-                                      contentPadding: EdgeInsets.symmetric(
-                                        vertical: 16.h,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 20.h,
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 54.w),
-                                child: Text(
-                                  emailError ?? "",
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontSize: 12.sp,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-
-                        /// PASSWORD LABEL
-                        Transform.translate(
-                          offset: Offset(0, 15.h),
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 54.w),
-                            child: Text(
-                              "Password",
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 11.sp,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: 6.h),
-
-                        /// PASSWORD FIELD
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                buildIcon(Icons.lock),
-                                Expanded(
-                                  child: TextField(
-                                    controller: passwordController,
-                                    obscureText: obscurePassword,
-                                    decoration: InputDecoration(
-                                      border: const UnderlineInputBorder(),
-                                      contentPadding: EdgeInsets.symmetric(
-                                        vertical: 16.h,
-                                      ),
-                                      suffixIcon: IconButton(
-                                        icon: Icon(
-                                          obscurePassword
-                                              ? Icons.visibility_off
-                                              : Icons.visibility,
-                                          size: 22.sp,
-                                        ),
-                                        onPressed: () {
-                                          setState(() {
-                                            obscurePassword = !obscurePassword;
-                                          });
-                                        },
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: 20.h,
-                              child: Padding(
-                                padding: EdgeInsets.only(left: 54.w),
-                                child: Text(
-                                  passwordError ?? "",
-                                  style: TextStyle(
-                                    color: Colors.red,
-                                    fontSize: 12.sp,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-
-                        SizedBox(height: 20.h),
-
-                        /// FORGOT PASSWORD
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => const ForgotpassPage(),
-                                ),
-                              );
-                            },
-                            child: Text(
-                              "Forgot Password",
-                              style: TextStyle(fontSize: 14.sp),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: 30.h),
-
-                        /// LOGIN BUTTON
-                        SizedBox(
-                          width: double.infinity,
-                          height: 50.h,
-                          child: ElevatedButton(
-                            onPressed: validateLogin,
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFF875DFC),
-                              foregroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30.r),
-                              ),
-                            ),
-                            child: Text(
-                              "Log In",
-                              style: TextStyle(fontSize: 16.sp),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: 30.h),
-
-                        /// OR
-                        Center(
-                          child: Text(
-                            "or",
-                            style: TextStyle(
-                              fontSize: 18.sp,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(height: 30.h),
-
-                        /// GOOGLE LOGIN
-                        Center(
-                          child: InkWell(
-                            onTap: () {},
-                            borderRadius: BorderRadius.circular(50.r),
-                            child: Container(
-                              width: 52.w,
-                              height: 52.w,
-                              decoration: BoxDecoration(
-                                color: const Color(
-                                  0xFF875DFC,
-                                ).withOpacity(0.15),
-                                shape: BoxShape.circle,
-                              ),
-                              child: Center(
-                                child: Image.network(
-                                  "https://cdn-icons-png.flaticon.com/512/281/281764.png",
-                                  width: 26.w,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        const Spacer(),
-
-                        /// SIGN UP
-                        Center(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Don't have an account? ",
-                                style: TextStyle(fontSize: 14.sp),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          const RegisterPage(),
-                                    ),
-                                  );
-                                },
-                                child: Text(
-                                  "Sign Up",
-                                  style: TextStyle(fontSize: 14.sp),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        SizedBox(height: 50.h),
-                      ],
-                    ),
-                  ),
-                ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: "Email",
               ),
-            );
-          },
+            ),
+
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                labelText: "Password",
+              ),
+              obscureText: true,
+            ),
+
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: login,
+              child: Text("Login"),
+            )
+
+          ],
         ),
       ),
     );
